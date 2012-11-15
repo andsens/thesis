3dbfbbf18fa9b1aa8554e9069e99fa45bc67ae8d
Anders Ingemann <anders@ingemann.de>
2012-11-16 00:59:05 +0100

    almost working

--- Comb/parser/simple-parse.hs
+++ Comb/parser/simple-parse.hs
@@ -1,64 +1,84 @@
-{-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns, RecordWildCards, DeriveDataTypeable #-}
 module Main
 	where
 import Text.XML.Light.Input
 import Text.XML.Light.Types
-import Text.Regex.Posix
---import Text.Regex.Base
---import Debug.Trace
+import Text.Regex.PCRE
+import Text.JSON(showJSObject)
+import Text.JSON.Pretty(pp_js_object)
+import Text.JSON.Types(JSObject(JSONObject), JSValue(..), toJSString, set_field, get_field)
+import Data.Maybe(maybeToList)
+import Debug.Trace
 --import Data.Maybe(isJust)
 
-data Location = Loc Int Element deriving Show
-type Trail = [Location]
-
-data Substring = Substr { before :: Int, after :: Int}
-data Attribute = Attr { name :: String, substring :: Substring }
-data Position  = Attribute | Substring | Fullstring deriving Show
-
-data Map = Map { placeholder :: String, trail :: Trail, position :: Position } deriving Show
-type Function = String
-data CSSMap = CSS { locator :: String, path :: String, subselector :: Function } deriving Show
 
 main :: IO ()
 main = do
 	xml <- readFile "data/profile.hbs"
 	let templateMap = traverse [] 0 (parseXML xml)
-	print $ map toCSS templateMap
+	--mapM_ print . show $ map createSelector templateMap
+	let json = toJSON (JSONObject []) (map createSelector templateMap)
+	putStrLn $ show (pp_js_object json)
+	--putStrLn ""
 
-toCSS :: Map -> CSSMap
-toCSS Map{..} = CSS placeholder (foldr prependPath "" trail) ""
+toJSON :: JSObject JSValue -> [Selector] -> JSObject JSValue
+toJSON object (Selector {..}:xs) =
+	set_field newObject name $ JSArray ( (jQuery path subselector) : (case oldProp of { Just (JSArray x) -> x; _ -> []}) )
+	where
+		newObject = toJSON object xs
+		oldProp = get_field newObject name
+toJSON object [] = object
+
+jQuery :: CSSPath -> Function -> JSValue
+jQuery path fn = JSString (toJSString path)
+
+
+
+type CSSPath  = String
+type Function = String
+data Selector = Selector { name :: String, path :: CSSPath, subselector :: Function } deriving Show
+
+createSelector :: Map -> Selector
+createSelector Map{..} = Selector m_name (foldr prependPath "" trail) ""
 
 prependPath :: Location -> String -> String
 prependPath (Loc i (Element (QName {..}) _ _ _)) s
 	| length s > 0 = s++">"++qName++":nth-child(" ++ show (i+1) ++ ")"
 	| otherwise    = qName++":nth-child(" ++ show (i+1) ++ ")"
 
+
+
+data Location = Loc Int Element deriving Show
+type Trail = [Location]
+
 traverse :: Trail -> Int -> [Content] -> [Map]
 traverse trail index (x@(Elem el@Element{..}):xs) =
 	let
 		current  = inspect ((Loc index el):trail) index x
-		children = traverse ((Loc index el):trail) index elContent
+		children = traverse ((Loc index el):trail) 0 elContent
 		siblings = traverse trail (index+1) xs
-	in prependMaybe current children ++ siblings
+	in (maybeToList current) ++ children ++ siblings
 traverse trail index (x:xs) =
 	let
 		current  = inspect trail index x
 		siblings = traverse trail (index+1) xs
-	in prependMaybe current siblings
+	in (maybeToList current) ++ siblings
 traverse trail index [] = []
 
-prependMaybe :: Maybe a -> [a] -> [a]
-prependMaybe (Just x) xs = x:xs
-prependMaybe Nothing xs  = xs
+
+
+data Substring = Substr { before :: Int, after :: Int}
+data Attribute = Attr { attrName :: String, substring :: Substring }
+data Position  = Attribute | Substring | Fullstring deriving Show
+data Map = Map { m_name :: String, trail :: Trail, position :: Position } deriving Show
 
 inspect :: Trail -> Int -> Content -> Maybe Map
 inspect trail index (Elem el) = -- Check attributes
 	Nothing
 inspect trail index (Text CData{..}) -- Check substrings
-	| length match > 0 = Just (Map match trail Fullstring)
+	| length matches > 0 = Just (Map (last $ last matches) trail Fullstring)
 	| otherwise = Nothing
 	where
-		match = cdData =~ "{{[^}]+}}"
+		matches = (cdData =~ "{{([^}]+)}}" :: [[String]])
 inspect trail index (CRef string) = -- Do nothing
 	Nothing
-
