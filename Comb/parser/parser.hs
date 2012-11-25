c0632628b4b6bd3551db4d67ec48523a686c4896
Anders Ingemann <anders@ingemann.de>
2012-11-25 22:06:51 +0100

    AAARGH!

--- Comb/parser/parser.hs
+++ Comb/parser/parser.hs
@@ -5,10 +5,9 @@ import qualified Text.Parsec.Token as T
 import qualified Text.Parsec.Char as C
 import Text.Parsec.Language(emptyDef)
 import Text.Parsec.Combinator
---import CustomCombinators
+import Data.Functor.Identity(Identity)
 import Debug.Trace
 
---run :: Show a => Parsec a -> String -> IO ()
 run parser input =
 	case (parse parser "" input) of
 		Left err -> do
@@ -18,53 +17,67 @@ run parser input =
 
 file parser path = do
 	input <- readFile path
-	return (runParser parser () path input)
+	case (runParser parser () path input) of
+		Left err -> do
+			putStr "parse error at ";
+			print err
+		Right x  -> print x
+	return ()
 
 
-x_start_ops = ["<", "</"]
-x_end_ops   = [">", "/>"]
+x_start_ops = ["<", "</", "<!--"]
+x_end_ops   = [">", "/>", "-->"]
 m_start_ops = ["{{", "{{/", "{{{", "{{#", "{{^", "{{!", "{{>"]
 m_end_ops   = ["}}", "}}}"]
 
-lexer :: T.TokenParser ()
-lexer  = T.makeTokenParser (
+xml_lexer = T.makeTokenParser (
 	emptyDef {
-		T.opLetter        = oneOf "/=",
-		T.reservedOpNames = x_start_ops ++ x_end_ops ++ m_start_ops ++ m_end_ops,
+		T.nestedComments  = False,
+		T.opStart         = oneOf "<",
+		T.opLetter        = oneOf "/",
+		T.reservedOpNames = x_start_ops ++ x_end_ops,
 		T.caseSensitive   = False
 	})
 
-identifier    = T.identifier lexer
-symbol        = T.symbol lexer
-reservedOp    = T.reservedOp lexer
-operator      = T.operator lexer
-stringLiteral = T.stringLiteral lexer
+x_identifier    = T.identifier xml_lexer
+x_symbol        = T.symbol xml_lexer
+x_reservedOp    = T.reservedOp xml_lexer
+
+mustache_lexer = T.makeTokenParser (
+	emptyDef {
+		T.commentStart    = "{{!",
+		T.commentEnd      = "}}",
+		T.nestedComments  = False,
+		T.opStart         = oneOf "",
+		T.opLetter        = oneOf "",
+		--T.reservedOpNames = m_start_ops ++ m_end_ops,
+		T.caseSensitive   = False
+	})
+
+m_identifier    = T.identifier mustache_lexer
+m_reservedOp    = T.reservedOp mustache_lexer
+m_symbol        = T.symbol mustache_lexer
 
 {--
 content ::= {{  id  }}
           | {{{ id }}}
           | {{# id  }} content {{/ id }}
           | {{^ id  }} content {{/ id }}
-          | <id [id = str_content]* />
-          | <id [id = str_content]* > content </id>
+          | <id [id = string_content]* />
+          | <id [id = string_content]* > content </id>
+          | <!-- comment -->
           | string
 --}
 
 template = many any_content
 
 any_content =
-	    try m_escaped
-	<|> try m_unescaped
-	<|> try m_content_section
-	<|> try xml_empty_tag
-	<|> try xml_tag
-	<|> try node_data
-
-str_content =
-	    try m_escaped
-	<|> try m_unescaped
-	<|> try m_string_section
-	<|> try string_data
+	    m_section any_content
+	<|> m_unescaped
+	<|> m_escaped
+	<|> xml_comment
+	<|> xml_tag
+	<|> text_data (x_start_ops ++ m_start_ops)
 
 data Content =
 	Section {
@@ -78,6 +91,8 @@ data Content =
 		name :: String,
 		attributes :: [XMLAttribute],
 		content :: [Content]
+	} | XMLComment {
+		content :: [Content]
 	} | Text {
 		text :: String
 	} deriving (Show)
@@ -88,65 +103,81 @@ data XMLAttribute =
 		attr_content :: [Content]
 	} deriving (Show)
 
-m_escaped   = do { reservedOp  "{{"; name <- identifier; reservedOp "}}";  return $ Variable name True }
-m_unescaped = do { reservedOp "{{{"; name <- identifier; reservedOp "}}}"; return $ Variable name False }
+m_escaped   = do
+	m_reservedOp "{{" <?> "mustache variable"
+	name <- m_identifier
+	m_reservedOp "}}"
+	return $ Variable name True
+m_unescaped = do
+	m_reservedOp "{{{" <?> "mustache variable (unescaped)"
+	name <- m_identifier
+	m_reservedOp "}}}"
+	return $ Variable name False
 
-m_content_section = do
-	inverted <- ( do { try $ reservedOp "{{#"; return False }
-	          <|> do { try $ reservedOp "{{^"; return True } )
-	name <- identifier
-	reservedOp "}}"
-	content <- many any_content
-	reservedOp "{{/"; C.string name; reservedOp "}}"
+m_section allowed_content = do
+	inverted <-
+		do
+			m_reservedOp "{{#" <?> "mustache section"
+			return False
+		<|> do
+			m_reservedOp "{{^" <?> "mustache section (inverted)"
+			return True
+	name <- m_identifier
+	m_reservedOp "}}"
+	content <- many allowed_content
+	m_reservedOp "{{/"; C.string name; m_reservedOp "}}"
 	return (Section name inverted content)
 
-m_string_section = do
-	inverted <- ( do { try $ reservedOp "{{#"; return False }
-	          <|> do { try $ reservedOp "{{^"; return True } )
-	name <- identifier
-	reservedOp "}}"
-	content <- many str_content
-	reservedOp "{{/"; C.string name; reservedOp "}}"
-	return (Section name inverted content)
 
-xml_empty_tag = do
-	reservedOp "<";
-	tag_name <- identifier
-	attrs <- xml_attribute `sepBy` (many1 C.space)
-	reservedOp "/>";
-	return (XMLTag tag_name attrs [])
+comment_content =
+	    m_section comment_content
+	<|> m_unescaped
+	<|> m_escaped
+	<|> text_data ("-->":m_start_ops)
+
+xml_comment =
+	do
+		x_reservedOp "<!--"
+		content <- many comment_content
+		x_reservedOp "-->"
+		return $ XMLComment content
 
 xml_tag = do
-	reservedOp "<";
-	tag_name <- identifier
-	attrs <- xml_attribute `sepBy` (many1 C.space)
-	reservedOp ">";
-	content <- many any_content
-	reservedOp "</"; C.string tag_name; reservedOp ">"
-	return (XMLTag tag_name attrs content)
+	x_reservedOp "<"
+	name <- x_identifier
+	attrs <- many xml_attribute
+	content <-
+		do 
+			x_reservedOp "/>"
+			return []
+		<|> do
+			x_reservedOp ">"
+			content <- many any_content
+			x_reservedOp "</"
+			C.string name
+			x_reservedOp ">"
+			return content
+	return (XMLTag name attrs content)
+
+string_content =
+	    m_section string_content
+	<|> m_unescaped
+	<|> m_escaped
+	<|> text_data ("\"":m_start_ops)
 
 xml_attribute = do
-	name <- identifier
-	symbol "="
-	value <- between (symbol "\"") (symbol "\"") (many str_content)
+	name <- x_identifier
+	x_symbol "="
+	value <- between (x_symbol "\"") (x_symbol "\"") (many string_content)
 	return $ XMLAttribute name value
 
-node_data =
+text_data disallowed_operators =
 	do
-		let ops = map C.string (x_start_ops ++ m_start_ops)
+		let ops = map C.string disallowed_operators
 		notFollowedBy (choice ops)
 		first <- anyChar
 		rest <- manyTill anyChar ( eof <|> do { choice . map lookAhead $ ops; return () } )
 		return $ Text (first:rest)
-
-string_data =
-	do
-		let ops = map C.string (x_start_ops ++ m_start_ops)
-		notFollowedBy (choice ops)
-		first <- anyChar
-		rest <- manyTill anyChar ( choice . map lookAhead $ (C.string "\""):ops )
-		return $ Text (first:rest)
-
 
 
 
