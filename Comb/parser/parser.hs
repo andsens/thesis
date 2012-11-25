b201ce6e88b5875f6e7bb84f948ee070ae81f0bd
Anders Ingemann <anders@ingemann.de>
2012-11-25 16:54:28 +0100

    bakcup

--- Comb/parser/parser.hs
+++ Comb/parser/parser.hs
@@ -39,75 +39,63 @@ x_end_ops   = [">", "/>"]
 m_start_ops = ["{{", "{{/", "{{{", "{{#", "{{^", "{{!", "{{>"]
 m_end_ops   = ["}}", "}}}"]
 
-data Text = Data String deriving (Show)
-node_data =
-	do
-		let ops = map C.string (x_start_ops ++ m_start_ops)
-		notFollowedBy (choice ops)
-		first <- anyChar
-		rest <- manyTill anyChar (choice . map lookAhead $ ops)
-		return $ Data (first:rest)
+data Mustache =
+	Section {
+		name :: String,
+		inverted :: Bool,
+		content :: [Content]
+	} | Variable {
+		name :: String,
+		escaped :: Bool
+	} deriving (Show)
 
-data GenVariable =
-	  Escaped String
-	| Unescaped String
-	| Comment String
-	| Partial String
-	deriving (Show)
-
-m_escaped   = do { reservedOp  "{{"; var_name <- identifier; reservedOp "}}";  return $ Escaped var_name }
-m_unescaped = do { reservedOp "{{{"; var_name <- identifier; reservedOp "}}}"; return $ Unescaped var_name }
-
-
-data GenSection =
-	  NormalSection String
-	| InvertedSection String
-	deriving (Show)
+m_escaped   = do { reservedOp  "{{"; var_name <- identifier; reservedOp "}}";  return $ Variable var_name True }
+m_unescaped = do { reservedOp "{{{"; var_name <- identifier; reservedOp "}}}"; return $ Unescaped var_name False }
 
 m_content_section = do
-	trace "open" $ reservedOp "{{#"; var_name <- identifier; reservedOp "}}"
+	reservedOp "{{#"; var_name <- identifier; reservedOp "}}"
 	--trees <- content
 	reservedOp "{{/"; C.string var_name; reservedOp "}}"
-	return (NormalSection var_name)
+	return (Section var_name False [])
 
 m_content_inverted = do
 	reservedOp "{{^"; var_name <- identifier; reservedOp "}}"
 	--trees <- content
 	reservedOp "{{/"; C.string var_name; reservedOp "}}"
-	return (InvertedSection var_name)
-
-
-data Mustache =
-	  Section GenSection
-	| Variable GenVariable
-	deriving (Show)
+	return (Section var_name True [])
 
 mustache_content =
-	    do {section <- try m_content_section; return $ Section section}
-	<|> do {section <- try m_content_inverted; return $ Section section}
-	<|> do {variable <- try m_escaped; return $ Variable variable}
-	<|> do {variable <- try m_unescaped; return $ Variable variable}
+	    try m_content_section
+	<|> try m_content_inverted
+	<|> try m_escaped
+	<|> try m_unescaped
 
 
-data XMLAttribute = XMLAttribute String deriving (Show)
+data XMLAttribute =
+	XMLAttribute {
+		attr_name :: String,
+		attr_content :: [StrContent]
+	} deriving (Show)
 
 xml_attribute = do
 	name <- identifier
 	symbol "="
 	value <- stringLiteral  -- for now "between" maybe?
-	return $ XMLAttribute name
+	return $ XMLAttribute name []
 
-data EmptyXMLTag = EmptyXMLTag String deriving (Show)
+data XMLTag =
+	XMLTag {
+		name :: String,
+		attributes :: [XMLAttribute],
+		content :: [Content]
+	} deriving (Show)
 
 xml_empty_tag = do
 	reservedOp "<";
 	tag_name <- identifier
 	attrs <- sepBy (many1 C.space) xml_attribute
 	reservedOp "/>";
-	return (EmptyXMLTag tag_name)
-
-
-data XMLTag = XMLTag String deriving (Show)
+	return (XMLTag tag_name [] [])
 
 xml_tag = do
 	reservedOp "<";
@@ -116,89 +104,37 @@ xml_tag = do
 	reservedOp ">";
 	--trees <- content
 	reservedOp "</"; C.string tag_name; reservedOp ">"
-	return (XMLTag tag_name)
+	return (XMLTag tag_name [] [])
 
-data XML =
-	  EmptyTag EmptyXMLTag
-	| NormalTag XMLTag
-	deriving (Show)
-
-xml_content =
-	    do {tag <- try xml_empty_tag; return $ EmptyTag tag}
-	<|> do {tag <- try xml_tag; return $ NormalTag tag}
+xml_content = try xml_empty_tag <|> try xml_tag
 
 data Content =
 	  MustacheContent Mustache
-	| XMLContent XML
+	| XMLContent XMLTag
 	| TextContent Text
 	deriving (Show)
 
+
+data Text = Data String deriving (Show)
+node_data =
+	do
+		let ops = map C.string (x_start_ops ++ m_start_ops)
+		notFollowedBy (choice ops)
+		first <- anyChar
+		rest <- manyTill anyChar (choice . map lookAhead $ ops)
+		return $ Data (first:rest)
+{--
+content ::= {{  id  }}
+          | {{{ id }}}
+          | {{# id  }} content {{/ id }}
+          | {{^ id  }} content {{/ id }}
+          | <id [id = del_content]* />
+          | <id [id = del_content]* > content </id>
+          | string
+--}
 content = do
-	    do {mst <- trace "mustache" $ try mustache_content; return $ MustacheContent mst}
-	<|> do {xml <- trace "xml" $ try xml_content; return $ XMLContent xml}
-	<|> do {txt <- trace "text" $ try node_data; return $ TextContent txt}
+	    do {mst <- try mustache_content; return $ MustacheContent mst}
+	<|> do {xml <- try xml_content; return $ XMLContent xml}
+	<|> do {txt <- try node_data; return $ TextContent txt}
 
 template = many content
-
---mst_unescaped  ::= mst_open "{" mst_name "}" mst_close
---mst_section    ::= mst_open "#" mst_name     mst_close content* mst_open "/" mst_name mst_close
---mst_inverted   ::= mst_open "^" mst_name     mst_close content* mst_open "/" mst_name mst_close
---mst_section_s  ::= mst_open "#" mst_name     mst_close string*  mst_open "/" mst_name mst_close
---mst_inverted_s ::= mst_open "^" mst_name     mst_close string*  mst_open "/" mst_name mst_close
---mst_comment    ::= mst_open "!" mst_comment  mst_close
---mst_partial    ::= mst_open ">" mst_name     mst_close
-
-{--
-string = mst_s <|> text
-
-xml_attr_value = many string
-
-xml_attr =
-	do {
-		name  <- xml_attr_name
-		value <- xml_attr_value
-		return $ XMLAttr name value
-	}
-
-
-
-
-template :: Parser [Content]
-template =
-	do {
-		trees <- many content;
-		return trees
-	}
-
-content =
-	do {
-		xml <- xml;
-		return xml
-	} <|> do {
-		mst <- mst;
-		return mst
-	} <|> do {
-		text <- text
-	}
-
-
-
-
-
-produkt = do{ reserved "return"
-            ; p <- price
-            ; semi
-            ; return (-p)
-            }
-      <|> do{ identifier
-            ; p  <- price
-            ; semi
-            ; return p
-            }
-      <?> "produkt"
-
-total   = do{ p <- price
-            ; reserved "total"
-            ; return p
-            }
---}
