c8749269eb53069693664d805b974a612917ca9a
Anders Ingemann <anders@ingemann.de>
2013-01-14 01:38:16 +0100

    Beatuiful package is created now

--- create-archive.sh
+++ create-archive.sh
@@ -1,6 +1,33 @@
-#!/bin/bash -xe
-(cd prototype; git archive --format=tar.gz --prefix=prototype HEAD > ../thesis-package/prototype.tar.gz)
-(cd Comb; git archive --format=tar.gz --prefix=prototype HEAD > ../thesis-package/comb.tar.gz)
+#!/bin/bash -e
+mkdir thesis-package
+rm -rf thesis-package.tar.gz thesis-package/*
+
+(cd prototype && git archive --prefix=prototype/ -o ../thesis-package/prototype.tar HEAD)
+(cd prototype/lib/php-activerecord && git archive --prefix=php-activerecord/ -o ../../../thesis-package/php-activerecord.tar HEAD)
+(cd Comb && git archive --prefix=Comb/ -o ../thesis-package/comb.tar HEAD)
+
+cd thesis-package
+tar -xf prototype.tar
+tar -xf comb.tar
+
+cd Comb
+rm Editor/app/scripts/comb
+cp -r parser Editor/app/scripts/comb
+
+rm MovieDatabase/client/scripts/vendor/comb
+cp -r parser MovieDatabase/client/scripts/vendor/comb
+
+cp -r ../../Comb/MovieDatabase/server/vendor MovieDatabase/server
+
+cd ..
+
+cd prototype/lib
+tar -xf ../../php-activerecord.tar
+cd ../..
+
+rm *.tar
+cd ..
+
 cp thesis/thesis.*.pdf thesis-package/
-tar -cvf thesis-package.tar.gz thesis-package
+tar --exclude=.DS_Store -cf thesis-package.tar.gz thesis-package
 sha1sum thesis-package.tar.gz
