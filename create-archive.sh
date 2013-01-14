cca685a5554e0f7c5ede0260b5a6cda5d948001c
Anders Ingemann <anders@ingemann.de>
2013-01-14 01:06:25 +0100

    Working on the download package

--- create-archive.sh
+++ create-archive.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-git archive --format=tar.gz --prefix=. HEAD Comb prototype thesis/thesis.*.pdf > archive.tar.gz
+#!/bin/bash -xe
+(cd prototype; git archive --format=tar.gz --prefix=prototype HEAD > ../thesis-package/prototype.tar.gz)
+(cd Comb; git archive --format=tar.gz --prefix=prototype HEAD > ../thesis-package/comb.tar.gz)
+cp thesis/thesis.*.pdf thesis-package/
+tar -cvf thesis-package.tar.gz thesis-package
+sha1sum thesis-package.tar.gz
